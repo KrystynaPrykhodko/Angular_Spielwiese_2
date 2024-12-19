484366a4893731e795cd833bb64bfa64b7d3b8eb
Krystyna Prykhodko <krystyna.prykhodko@gmail.com>
2024-12-19 13:31:41 +0100

    add hamburger.component; simle html for sidebar and create

---
+++ Angular_Spielwiese_2/src/app/Components/hamburger/hamburger.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hamburger',
+  imports: [],
+  templateUrl: './hamburger.component.html',
+  styleUrl: './hamburger.component.css'
+})
+export class HamburgerComponent {
+
+}
