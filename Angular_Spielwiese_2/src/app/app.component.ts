0ca3465143a79e45a70b9d2cdf16d8c0a1339bbb
Krystyna Prykhodko <krystyna.prykhodko@gmail.com>
2024-12-19 12:43:49 +0100

    create new copmponents

--- Angular_Spielwiese_2/src/app/app.component.ts
+++ Angular_Spielwiese_2/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './Components/sidebar/sidebar.component';
+import { CreateComponent } from './Components/create/create.component';
+import { TableComponent } from './Components/table/table.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CreateComponent, TableComponent, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
