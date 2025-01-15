868ae3150c999b91397a5588e931f4b2b9e4f922
Krystyna Prykhodko <krystyna.prykhodko@gmail.com>
2025-01-15 12:36:16 +0100

    Clear feedbacks branch for review

--- Angular_Spielwiese_2/src/app/store/app.state.ts
+++
@@ -1,5 +0,0 @@
-import { BooksState } from './books/books.state';
-
-export interface AppState {
-  books: BooksState;
-}
--- Angular_Spielwiese_2/src/app/store/books/books.actions.ts
+++
@@ -1,8 +0,0 @@
-import { createAction, props } from '@ngrx/store';
-import { Book } from '../../models/book.model';
-
-// Optionale Actions für zukünftige Erweiterungen
-export const loadBooks = createAction('[Books] Load Books');
-export const createBook = createAction('[Books] Create Book', props<{ book: Book }>());
-export const editBook = createAction('[Books] Edit Book', props<{ book: Book }>());
-export const deleteBook = createAction('[Books] Delete Book', props<{ bookId: number }>())
