e268454966dc8ed5723fd1e8888e8e132e2d9d08
Krystyna Prykhodko <krystyna.prykhodko@gmail.com>
2025-01-15 11:07:08 +0100

    commit test

--- Angular_Spielwiese_2/src/app/store/books/books.selectors.ts
+++ Angular_Spielwiese_2/src/app/store/books/books.selectors.ts
@@ -4,7 +4,6 @@ import { BooksState } from './books.state';
 // Selektiere den gesamten Books-State
 export const selectBooksState = createFeatureSelector<BooksState>('books');
 
-// Selektiere die Bücher aus dem State
 export const selectAllBooks = createSelector(
   selectBooksState,
   (state) => state.books
