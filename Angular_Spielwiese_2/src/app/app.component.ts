6715cdc7fe0c1405944d1410775ffbb3a359a4de
Krystyna Prykhodko <krystyna.prykhodko@gmail.com>
2025-02-10 13:38:46 +0100

    solve error

--- Angular_Spielwiese_2/src/app/app.component.ts
+++ Angular_Spielwiese_2/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { HamburgerComponent } from "./components/hamburger/hamburger.component";
+import { SidebarComponent } from './Components/sidebar/sidebar.component';
+import { HamburgerComponent } from "./Components/hamburger/hamburger.component";
 
 
 @Component({
