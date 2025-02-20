d3b3c726013e6569340cd154d553b98accfc76df
Krystyna Prykhodko <krystyna.prykhodko@gmail.com>
2025-02-20 10:11:32 +0100

    test SidebarComponent - should render the component

--- Angular_Spielwiese_2/src/app/Components/sidebar/sidebar.component.spec.ts
+++ Angular_Spielwiese_2/src/app/Components/sidebar/sidebar.component.spec.ts
@@ -1,1 +1,17 @@
 
+import { TestBed } from '@angular/core/testing';
+import { SidebarComponent } from './sidebar.component';
+
+describe('SidebarComponent', () => {
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [SidebarComponent],
+        }).compileComponents();
+    });
+        it('should render the component', () => {
+            const fixture = TestBed.createComponent(SidebarComponent);
+            fixture.detectChanges();
+            expect(fixture.componentInstance).toBeTruthy();
+        });
+    
+});
