0ef461efcf40401bde8d404a1ea74e01de8d531d
Krystyna Prykhodko <krystyna.prykhodko@gmail.com>
2025-02-26 15:38:09 +0100

    create test 'open form when clicking button' for button.component

--- Angular_Spielwiese_2/src/app/button/button.component.spec.ts
+++ Angular_Spielwiese_2/src/app/button/button.component.spec.ts
@@ -1,17 +1,36 @@
 import { TestBed } from "@angular/core/testing";
 import { ButtonComponent } from "./button.component";
+import { By } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 
 describe('AppComponent', () => {
+  let router: Router;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [ButtonComponent],
     }).compileComponents();
+
+    router = TestBed.inject(Router);
   });
 
   it('should render the component', () => {
     const fixture = TestBed.createComponent(ButtonComponent);
     fixture.detectChanges();
     expect(fixture.componentInstance).toBeTruthy();
+  });
+
+  it('should navigate to "/create" when clicking the "Create" button', () => {
+    const fixture = TestBed.createComponent(ButtonComponent);
+    fixture.detectChanges();
+
+    const navigateSpy = jest.spyOn(router, 'navigate'); // Überwacht Router-Navigation
+    
+    const buttonCreate = fixture.debugElement.query(By.css('#buttonCreate')).nativeElement;
+    buttonCreate.dispatchEvent(new Event('click'));
+    fixture.detectChanges();
+
+    expect(navigateSpy).toHaveBeenCalledWith(['/create']);
   });
 });
 
