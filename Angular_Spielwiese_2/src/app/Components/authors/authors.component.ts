fbf3f7e927146cad9902ebabff3bc77b8381faef
Krystyna Prykhodko <krystyna.prykhodko@gmail.com>
2025-03-03 15:59:34 +0100

    create destroy to avoid dataLeck for autohr.component

--- Angular_Spielwiese_2/src/app/Components/authors/authors.component.ts
+++ Angular_Spielwiese_2/src/app/Components/authors/authors.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OpenAPIService } from '../../services/openAPI.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-authors',
@@ -8,15 +9,16 @@ import { OpenAPIService } from '../../services/openAPI.service';
   templateUrl: './authors.component.html',
   styleUrls: ['./authors.component.css']
 })
-export class AuthorsComponent implements OnInit {
+export class AuthorsComponent implements OnInit, OnDestroy {
   // alles zur API
   authorsList: string[] = [];
+  private subscription: Subscription = new Subscription();
 
   constructor(private openAPIService: OpenAPIService) {}
 
   ngOnInit(): void {       
     // API aufrufen
-    this.openAPIService.fetchData().subscribe((data) => {                   
+    this.subscription = this.openAPIService.fetchData().subscribe((data) => {                   
       // alle Autoren in authorsList speichern
       data.docs.forEach((book: any) => {                       
         if (book.author_name) {                                             
@@ -28,5 +30,9 @@ export class AuthorsComponent implements OnInit {
       //console.log(this.authorsList);
     });
   }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+}
 }
 
